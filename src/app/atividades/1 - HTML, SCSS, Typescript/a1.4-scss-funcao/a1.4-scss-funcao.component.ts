378ae209099ac8720c16dc08d3321908c0cce195
Switch1511 <gustavopasqualli0@gmail.com>
2024-10-10 21:34:00 -0300

    Resolucao atividade a1.4

--- src/app/atividades/1 - HTML, SCSS, Typescript/a1.4-scss-funcao/a1.4-scss-funcao.component.ts
+++ src/app/atividades/1 - HTML, SCSS, Typescript/a1.4-scss-funcao/a1.4-scss-funcao.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./a1.4-scss-funcao.component.scss']
 })
 export class A14ScssFuncaoComponent implements OnInit {
+  items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   constructor() { }
 
